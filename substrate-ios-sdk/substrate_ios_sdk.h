3252e618e9b9cdff9be129f3b2cef3449a8584b3
Denis Lebedko <Viscount88@gmail.com>
2022-12-13 12:41:21 +0700

    Initial Commit

---
+++ substrate-ios-sdk/substrate_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  substrate_ios_sdk.h
+//  substrate-ios-sdk
+//
+//  Created by Денис Лебедько on 13.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for substrate_ios_sdk.
+FOUNDATION_EXPORT double substrate_ios_sdkVersionNumber;
+
+//! Project version string for substrate_ios_sdk.
+FOUNDATION_EXPORT const unsigned char substrate_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <substrate_ios_sdk/PublicHeader.h>
+
+
